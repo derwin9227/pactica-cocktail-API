ec2ed0fa335f7d44c2f64f8f73e02df42e4fd311
derwin <derwin9227@gmail.com>
2023-02-12 16:18:44 -0500

    renderizando busqueda

--- API-connect.js
+++ API-connect.js
@@ -2,11 +2,15 @@
 strDrink
 strAlcoholic
 strInstructions
-strImageSource
+strDrinkThumb
  */
 const inputCoctel = document.getElementById("inputCoctel");
 const botonBuscar =  document.getElementById("buscarCoctel");
-
+const coctel = document.getElementById("coctel");
+const templateCoctel = (nombreCoctel, instrucciones, imagenCoctel) => 
+        `<h2>${nombreCoctel}</h2>
+        <img src="${imagenCoctel}" alt="${nombreCoctel}">
+        <p>${instrucciones}</p>`;
 
 const apiConnect = (inputCoctel) => fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${inputCoctel}`)
     .then(res => res.json())
@@ -14,6 +18,7 @@ const apiConnect = (inputCoctel) => fetch(`https://thecocktaildb.com/api/json/v1
         if(cocteles.drinks === null){
             console.log(`no hay datos del siguiente coctel -> ${inputCoctel} <-`)}
         else{
+            coctel.innerHTML = templateCoctel(cocteles.drinks[0].strDrink, cocteles.drinks[0].strInstructions, cocteles.drinks[0].strDrinkThumb);
         console.log(`nombre del coctel -${cocteles.drinks[0].strDrink}- bebida tipo +${cocteles.drinks[0].strAlcoholic}+`)}
         })
     .catch(e => console.log(e))
