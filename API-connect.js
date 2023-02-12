008f0822ed97578ab5b0152b76c6c85ef52207b7
derwin <derwin9227@gmail.com>
2023-02-12 14:45:26 -0500

    conectando la API de cocteles

--- API-connect.js
+++ API-connect.js
@@ -1,1 +1,7 @@
 //www.thecocktaildb.com/api/json/v1/1/search.php?s=
+fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then(res => res.json())
+    .then(cocteles => console.log(`nombre del coctel -${cocteles.drinks[0].strDrink}- bebida tipo +${cocteles.drinks[0].strAlcoholic}+`))
+    .catch(e => e)
+
+    //console.log(`nombre del coctel ${cocteles.drink[0].strDrink} bebida tipo ${cocteles.drink[0].strAlcoholic}`)
