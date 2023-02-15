0be78e2ba5e873ba85216cd28d2c9b07a7fbf573
derwin <derwin9227@gmail.com>
2023-02-15 00:20:39 -0500

    mejora en la busqueda para mostrar varios

--- API-connect.js
+++ API-connect.js
@@ -7,6 +7,7 @@ strDrinkThumb
 const inputCoctel = document.getElementById("inputCoctel");
 const botonBuscar =  document.getElementById("buscarCoctel");
 const coctel = document.getElementById("coctel");
+const limpiarBusqueda = () => coctel.innerHTML ="";
 const templateCoctel = (nombreCoctel, instrucciones, imagenCoctel) => 
         `<h2>${nombreCoctel}</h2>
         <img src="${imagenCoctel}" alt="${nombreCoctel}">
@@ -15,17 +16,18 @@ const templateCoctel = (nombreCoctel, instrucciones, imagenCoctel) =>
 const apiConnect = (inputCoctel) => fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${inputCoctel}`)
     .then(res => res.json())
     .then(cocteles => {
-        if(cocteles.drinks === null){
-            console.log(`no hay datos del siguiente coctel -> ${inputCoctel} <-`)}
+        limpiarBusqueda();
+        if(cocteles.drinks === null || inputCoctel === ""){
+            alert(`no hay datos del siguiente coctel -> ${inputCoctel} <-`)}
         else{
-            coctel.innerHTML = templateCoctel(cocteles.drinks[0].strDrink, cocteles.drinks[0].strInstructions, cocteles.drinks[0].strDrinkThumb);}
+            cocteles.drinks.forEach(item => {
+                coctel.innerHTML += templateCoctel(item.strDrink, item.strInstructions, item.strDrinkThumb);
+            });
+            }
         })
     .catch(e => console.log(e))
 
 const buscarCoctel = (nombreCoctel) => apiConnect(inputCoctel.value);
 botonBuscar.addEventListener("click", () => buscarCoctel());
-botonBuscar.addEventListener("mouseover", () => {botonBuscar.style.backgroundColor = "#0000df"; botonBuscar.style.color = "white"});
-
-botonBuscar.addEventListener("mouseleave", () => {botonBuscar.style.backgroundColor = "dimgray"; botonBuscar.style.color = "black"});
 
 inputCoctel.addEventListener("keypress", e => e.key==="Enter" ? buscarCoctel() : "");
